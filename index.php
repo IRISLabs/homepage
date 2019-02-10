095881b54e6c5f6ed94cc5e0ba68583f2064b748
Fadli Zul Fahmi <25176229+iFadli@users.noreply.github.com>
2019-02-10 23:22:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en" class="silver font f5-m f4-l lh-copy">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="index,follow">
+
+	<title>IRIS Labs | Integrated Research of Information System</title>
+
+	<meta name="description" content="">
+
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:site_name" content="">
+	<meta property="og:locale" content="en_US">
+
+	<meta name="twitter:card" content="">
+	<meta name="twitter:site" content="">
+	<meta name="twitter:creator" content="">
+	<meta name="twitter:url" content="">
+	<meta name="twitter:title" content="">
+	<meta name="twitter:description" content="">
+
+	<meta name="theme-color" content="#2176ff" />
+
+	<link rel="shortcut icon" href="">
+	<link rel="apple-touch-icon-precomposed" href=">
+
+	<link as="font" href="/assets/fonts/Inter-UI-Regular.woff2" rel="preload" type="font/woff2" crossorigin>
+	<link as="font" href="/assets/fonts/Inter-UI-Bold.woff2" rel="preload" type="font/woff2" crossorigin>
+
+	<link href="/assets/css/main.min.css" rel="stylesheet">
+</head>
+
+
+<body>
+	<header id="particles-js" class="w-100 flex items-center" style="height:100vh;">
+		<nav class="nav pv3 fixed top-0 w-100 bg-white z-3">
+			<ul class="pl0 mv0 flex justify-center center mw7">
+				<li class="nav-item mh3">
+					<a class="nav-link nav-link-1" href="#vision">vision</a>
+				</li>
+				<li class="nav-item mh3">
+					<a class="nav-link nav-link-2" href="#mission">mission</a>
+				</li>
+				<li class="nav-item mh3">
+					<a class="nav-link nav-link-3" href="#team">team</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="absolute w-100 tc">
+			<h1 class="f1 tracked lh-title sky mv0">IRIS Labs</h1>
+			<h4 class="f4 fw5 lh-solid  mv0">Integrated Research of Information System</h4>
+		</div>
+	</header>
+
+	<main>
+		<!-- vision -->
+		<section id="vision" class="pv4">
+			<div class="center mw7">
+				<header>
+					<h2 class="tc f2 lh-solid sky">vision</h2>
+				</header>
+				<blockquote class="tc">
+					<p>Putting forward student's self-actualization from the academic environment in order to deliver  impact on people's live.</p>
+				</blockquote>
+			</div>
+		</section>
+		<!-- end of vision -->
+
+		<!-- mission -->
+		<section id="mission" class="pv4">
+			<div class="center mw7">
+				<header>
+					<h2 class="tc f2 lh-solid sky">mission</h2>
+				</header>
+				<ul class="mission-list pl0 mv0">
+					<li class="mission-item flex flex-column flex-row-ns items-center pv4 pv0-ns tc tl-ns">
+						<h4 class="mission-short w-80 w-40-ns f4 ttl pa4 ma0 bg-sky white tc">Fostering</h4>
+						<span class="w-20 dn db-ns tc f4 sky2">→</span>
+						<div class="mission-long w-80 w-40-ns pa0 pa4-ns">
+							<p>Build an accessible research and development study group as part of
+								<strong>"Tridharma Perguruan Tinggi."</strong>
+							</p>
+						</div>
+					</li>
+					<li class="mission-item flex flex-column flex-row-ns items-center pv4 pv0-ns tc tl-ns flip">
+						<h4 class="mission-short w-80 w-40-ns f4 ttl pa4 ma0 bg-sky white tc flop">Enhancing</h4>
+						<span class="w-20 dn db-ns tc f4 sky2">→</span>
+						<div class="mission-long w-80 w-40-ns pa0 pa4-ns flop">
+							<p>Enhance our competency in the field of information system and technology to create a top notch quality research.</p>
+						</div>
+					</li>
+					<li class="mission-item flex flex-column flex-row-ns items-center pv4 pv0-ns tc tl-ns">
+						<h4 class="mission-short w-80 w-40-ns f4 ttl pa4 ma0 bg-sky white tc">Contributing</h4>
+						<span class="w-20 dn db-ns tc f4 sky2">→</span>
+						<div class="mission-long w-80 w-40-ns pa0 pa4-ns">
+							<p>Research and development as a standard of Information System Program of Indonesia Computer University.</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<!-- end of mission -->
+
+		<!-- team -->
+		<section id="team" class="bg-near-white pv4">
+			<div class="center mw7">
+				<header>
+					<h2 class="tc f2 lh-solid sky">team</h2>
+				</header>
+				<ul class="team-list grid pa4 mt0 mb3 tc">
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/firman.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Firman</h5>
+						<span class="f6">1st gen CEO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/izza.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Izza</h5>
+						<span class="f6">1st gen CLO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/taufik.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Taufik</h5>
+						<span class="f6">1st gen CTO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/stiven.jpg" alt="">
+						</figure>
+						<span class="f5 fw7 sky db mv0">Stiven</span>
+						<span class="f6">1st gen CO-CEO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/fadli.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Fadli</h5>
+						<span class="f6">2nd gen CEO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/bram.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Brama</h5>
+						<span class="f6">2nd gen
+							<span class="">CLO</span>
+						</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/nisa.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Chairunnisa</h5>
+						<span class="f6">2nd gen CFO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/dalih.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Dalih</h5>
+						<span class="f6">2nd gen CMO</span>
+					</li>
+					<li class="team-item">
+						<figure class="team-figure center relative overflow-hidden br-100 mw4-l ma0">
+							<img class="team-img" src="/assets/img/team/222208.jpg" alt="">
+						</figure>
+						<h5 class="f5 fw7 sky mt2 mb0">Khamil</h5>
+						<span class="f6">2nd gen CTO</span>
+					</li>
+					<!-- li class="team-item join relative bg-sky white pa3 flex flex-column justify-center">
+						<a class="absolute top-0 left-0 w-100 h-100" href="https://goo.gl/forms/m81UmqjAZxjcLMTC2" target="_blank"></a>
+						<h5 class="f4 fw7 mt3 mv0">help us grow!</h5>
+						<span class="f6">fill the survey</span>
+					</li -->
+				</ul>
+			</div>
+		</section>
+		<!-- end of team -->
+	</main>
+
+	<footer class="bg-near-black pv5">
+		<div class="center mw7 tc">
+			<aside class="flex justify-center mb4">
+				<a class="mh2 social-icon flex br-100 bg-gold justify-center items-center" href="mailto:iris@email.unikom.ac.id"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" stroke="#ffb700" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></a>
+				<a class="mh2 social-icon flex br-100 bg-sky justify-center items-center" href="https://twitter.com"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></a>
+				<a class="mh2 social-icon flex br-100 bg-blue justify-center items-center" href="https://facebook.com"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg></a>
+				<a class="mh2 social-icon flex br-100 bg-pink justify-center items-center" href="https://instagram.com"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" stroke="#ff80cc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.5" y2="6.5"></line></svg></a>
+				<a class="mh2 social-icon flex br-100 bg-dark-blue justify-center items-center" href="https://linkedin.com"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#fff" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
+			</aside>
+			<small class="white">copyright &copy; 2017 - <?=date('Y');?></small>
+		</div>
+	</footer>
+	<script src="/assets/js/particles.min.js"></script>
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/app.min.js"></script>
+</body>
+
+</html>
